15d55402d16a756068aefd456b4b7d91647e76e9
hazoumanidhal100-debug <hazoumanidhal100@gmail.com>
2026-05-06 13:33:32 +0200

    Update EX1.java

--- EX1.java
+++ EX1.java
@@ -1,17 +1,54 @@
 
+class BankAccount {
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String ownerName, double balance) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance!");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner: " + ownerName + ", Balance: " + balance;
+    }
+}
+
 public class TestBankAccount {
     public static void main(String[] args) {
-
-        BankAccount account = new BankAccount("Omar", 1500);
-
-        // Operations
-        account.deposit(300);
-        account.withdraw(200);
-
-        // Change name
-        account.setHolderName("Youssef");
-
-        // Print result
-        System.out.println(account);
+        BankAccount account = new BankAccount("Ahmed", 1000.0);
+        account.deposit(500.0);
+        account.withdraw(200.0);
+        account.setOwnerName("Ali");
+        account.setBalance(1500.0);
+        System.out.println(account.toString());
     }
 }
