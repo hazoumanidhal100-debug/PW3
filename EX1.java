a599369f9762bd44ec14ab1bab04dd7dc334c7d9
hazoumanidhal100-debug <hazoumanidhal100@gmail.com>
2026-05-06 13:18:41 +0200

    Update EX1.java

--- EX1.java
+++ EX1.java
@@ -1,1 +1,17 @@
 
+public class TestBankAccount {
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount("Omar", 1500);
+
+        // Operations
+        account.deposit(300);
+        account.withdraw(200);
+
+        // Change name
+        account.setHolderName("Youssef");
+
+        // Print result
+        System.out.println(account);
+    }
+}
